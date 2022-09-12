377a5c492b1e6076b8b91bee815d978b1f118310
JuanMi <jmrodriguez@sicnova3d.com>
2022-09-12 12:35:42 +0200

    Creacion del componente DHL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DhlComponent } from './components/dhl/dhl.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DhlComponent
   ],
   imports: [
     BrowserModule,
