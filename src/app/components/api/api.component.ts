cf8d8dbce5ed10b616508d0a80ddd804cbc47794
santiagovm222 <santiago.vargasm829@gmail.com>
2022-09-19 09:56:19 +0200

    Mostrado de IDs llamando a php

--- src/app/components/api/api.component.ts
+++ src/app/components/api/api.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Attribute, Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CrudService } from '../../services/crud.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { async, Observable, toArray } from 'rxjs';
+import { isIdentifier } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-api',
-  templateUrl: './api.component.html',
+  templateUrl: `./api.component.html`,
   styleUrls: ['./api.component.css']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
 export class ApiComponent implements OnInit {
+  myGroup:any
 
-  constructor() { }
-
-  ngOnInit(): void {
+  url = "https://prueba.sicnova3d.com/CRUD/";
+  
+  prod={
+    id:""
   }
 
+  productos:any;
+  //myGroup:any;
+  data$:any;
+  constructor(private servicio:CrudService) { 
+    //this.productos={};
+  }
+/*
+  prodObservable = new Observable((observer) => {
+      this.productos = this.getProductos
+  });
+*/  
+  ngOnInit() {
+    /*this.myGroup = new FormGroup({
+      ids: new FormControl(this.getProductos())
+    });*/
+    //this.getProductos().subscribe((result:any)=>{this.productos=result;});
+    //this.prodObservable.subscribe()
+    this.getProductos();
+    console.log(this.productos);
+    //this.productos=this.getProductos().subscribe((result:any)=>{console.log(result)});
+    //this.productos=this.getProductos().subscribe((result:any) => {this.productos=result});
+    //this.http.get(`${this.url}callProducto.php`).forEach(value=>{this.productos=value});
+    
+  //this.myGroup = new FormGroup({id: new FormControl()});
+  };
+  
+  getProductos(){
+    //return this.servicio.recuperarTodos();
+    
+    // Funciona
+    this.servicio.recuperarTodos().subscribe(() => {
+      this.servicio.recuperarTodos().forEach(value=>{this.productos=value; console.log(Object.values(value)[0]["@attributes"]["id"])});
+    });
+    /* //Funciona
+    this.servicio.recuperarTodos().subscribe((result:object) => {this.productos=result});
+    this.servicio.recuperarTodos().forEach(value=>{this.productos=value});
+    /**/
+    /* //Funciona
+    this.servicio.recuperarTodos().subscribe((result:any) => {this.productos=result});
+    this.servicio.recuperarTodos().forEach(value=>{this.productos=value});
+    /**/
+    //this.productos=this.servicio.recuperarTodos() // No funciona
+  };
 }
--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 // import { Dhli } from './dhli';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
+  //myData: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-  url='https://development.sicnova3d.com/test/proyect/classes/'; // disponer url de su servidor que tiene las páginas PHP
-
+  //url='https://development.sicnova3d.com/test/proyect/classes/'; // disponer url de su servidor que tiene las páginas PHP
+  url = "https://prueba.sicnova3d.com/CRUD/";
+  data:any;
+  //data=new Object;
   constructor(private http: HttpClient) { }
 
-  // recuperarTodos() {
-  //   return this.http.get(`${this.url}recuperartodos.php`);
-  // }
+
+  recuperarTodos() {
+    //return this.http.get(`${this.url}recuperartodos.php`);
+    // this.http.get(`${this.url}callProducto.php`).subscribe((result:any)=>{this.myData.next(result)});
+    //this.http.get(`${this.url}callProducto.php`).subscribe((result:any)=>{this.data = result});
+    //this.data=this.http.get(`${this.url}callProducto.php`);
+    return this.http.get(`${this.url}callProducto.php`);
+    //return this.myData.asObservable();
+    //return this.data;
+  }
 
   alta(articulo:any) {
     return this.http.post(`${this.url}test.php`, JSON.stringify(articulo));
